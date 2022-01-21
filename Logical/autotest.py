92d87fe175ca3880e479024c3d0b03b0e903beb6
TpKotoba <tpkotoba@gmail.com>
2022-01-22 06:12:59 +0800

    Logical instr

---
+++ Logical/autotest.py
@@ -0,0 +1,26 @@
+import os, itertools, time, sys
+
+if __name__ == "__main__":
+
+    try:
+        comment = str(sys.argv[1])
+    except:
+        comment = ""
+
+    INSTRUCTION = []
+    with open("./Instructions.txt", mode="r") as FILEIN:
+        for line in FILEIN:
+            INSTRUCTION.append(line.split(" ")[0])
+    SHAPE = ["16B", "8B", "X", "W"]
+    STYLE = ["nop", "par"]
+
+    for instype in itertools.product(INSTRUCTION, SHAPE, STYLE):
+        os.system(f"python3 generator_{instype[2]}.py {instype[0]} {instype[1]} > meta_{instype[2]}-{instype[0]}-{instype[1]}.S")
+        os.system(f"make meta_{instype[2]}-{instype[0]}-{instype[1]}.bin")
+    logfile = f"RESULT-{comment}-{int(time.time())}.txt"
+    os.system(f'echo "" > {logfile}')
+    N = len(INSTRUCTION)*len(SHAPE)*len(STYLE)
+    for i, instype in enumerate(itertools.product(INSTRUCTION, SHAPE, STYLE)):
+        execfile = f"./meta_{instype[2]}-{instype[0]}-{instype[1]}.bin"
+        print(f"exec({i+1}/{N}): {execfile}")
+        os.system(f"{execfile} >> {logfile}")
---
+++ Logical/generator_nop.py
@@ -0,0 +1,46 @@
+import sys
+
+def print_code(instruction, shape):
+    if(shape == "X"):
+        r = ["x19", "x20", "x21", "x22"]
+        shape = ""
+    elif(shape == "W"):
+        r = ["w19", "w20", "w21", "w22"]
+        shape = ""
+    else:
+        r = ["v8", "v9", "v10", "v11"]
+        shape = "."+shape
+    print(f'''#include "macros.i"
+
+    .align  2
+    .global target
+#ifndef __clang__
+    .type   target, %function
+#endif
+
+    .macro core xx0, xx1, xx2, xx3, shape
+    {instruction} \\xx0\\shape, \\xx1\\shape, \\xx2\\shape
+    nop
+    .endm
+
+target:
+    push_all
+loop:
+    
+    Repeat256  core, {r[0]}, {r[1]}, {r[2]}, {r[3]}, {shape}
+    
+    subs    x0, x0, #1
+    bge     loop
+end:
+    pop_all
+    br  lr
+    ''')
+
+if __name__ == "__main__":
+    try: 
+        instr = sys.argv[1]
+        shape = sys.argv[2]
+    except:
+        instr = "AND"
+        shape = "8B"
+    print_code(instr, shape)
